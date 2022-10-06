b24fe715cf65a7370131d079898fe963bb63b02e
Gabriel <gabrielmartinslima@gmail.com>
2022-10-06 17:12:08 -0400

    Kill ffmpeg if code crashes

--- client/camera_hello_world copy.py
+++ client/camera_hello_world copy.py
@@ -2,12 +2,21 @@ from picamera2 import Picamera2
 from time import sleep
 import numpy as np
 import cv2 as cv
+import prctl
+import signal
 camera = Picamera2()
 camera.configure(camera.create_preview_configuration(main={"format": 'RGB888', "size": (640, 480)}))
 camera.start()
 #main.py
 
 import subprocess 
+
+class Classe:
+    def __init__(self):
+        print("construtor")
+
+    def __del__(self):
+        print("destrutor")
 
 command = ['ffmpeg',
             '-thread_queue_size', '1024',
@@ -34,17 +43,22 @@ command = ['ffmpeg',
             '-f', 'flv', 
             'rtmp://x.rtmp.youtube.com/live2/ect7-wqu9-tpce-qp22-d81u']
 
-pipe = subprocess.Popen(command, stdin=subprocess.PIPE)
-while True:
-    # Capture frame-by-frame
-    frame = camera.capture_array()
-    # Display the resulting frame
-    # cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
-    pipe.stdin.write(frame.tostring())
-    # sleep(0.1)
-# When everything done, release the capture
-pipe.kill()
-cv.destroyAllWindows()
-camera.stop()
+pipe = subprocess.Popen(command, stdin=subprocess.PIPE, preexec_fn=lambda: prctl.set_pdeathsig(signal.SIGKILL))
+
+try:
+    while True:
+        # Capture frame-by-frame
+        print("fluminense")
+        frame = camera.capture_array()
+        # Display the resulting frame
+        cv.imshow('frame', frame)
+        if cv.waitKey(1) == ord('q'):
+            break
+        pipe.stdin.write(frame.tostring())
+        # sleep(0.1)
+except:
+    # When everything done, release the capture
+    print("neymar")
+    pipe.kill()
+    cv.destroyAllWindows()
+    camera.stop()
---
+++ client/src/components/Camera.py
@@ -0,0 +1,7 @@
+class Camera:
+    
+    def __init__(self):
+        pass
+
+    def deinit(self):
+        pass
