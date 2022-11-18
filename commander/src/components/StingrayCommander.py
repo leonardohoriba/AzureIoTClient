93c8d4bdd4b098acff4ffe93210d80e8c7a4bb82
Gabriel Lima <gabrielmartinslima@gmail.com>
2022-11-18 16:32:27 -0500

    Commander move method and main for demonstration

--- commander/src/components/StingrayCommander.py
+++ commander/src/components/StingrayCommander.py
@@ -6,7 +6,7 @@ from src.utils.direct_method_constants import DeviceID, MethodName
 
 
 class StingrayCommander:
-    ROBOT_RADIUS = 204  # millimeters
+    ROBOT_RADIUS = 205  # millimeters
 
     def __init__(self, commander: Commander, robotNumber: int):
         self._commander = commander
@@ -39,6 +39,19 @@ class StingrayCommander:
                 "leftWheelDistance": angle
                 * (2 * pi / 360)
                 * (radius + self.ROBOT_RADIUS / 2),
+            },
+        )
+
+    def move(self, distance: float, speed: float):
+        self._commander.iothub_devicemethod(
+            device_id=self._deviceID,
+            method_name=MethodName.SET_MOVEMENT,
+            payload={
+                "instructionID": 4,
+                "rightWheelSpeed": abs(speed),
+                "rightWheelDistance": distance,
+                "leftWheelSpeed": abs(speed),
+                "leftWheelDistance": distance,
             },
         )
 
