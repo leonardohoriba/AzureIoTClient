eee6417885fb8d41e6cf14fe16b65c857db67f28
Leonardo Masato Souza Horiba <leonardo@ncct.uottawa.ca>
2022-11-24 17:28:37 -0500

    feature: socket and websocket connected

---
+++ commander/apps/stingray/consumers.py
@@ -0,0 +1,41 @@
+import json
+import time
+import threading
+from asgiref.sync import async_to_sync
+
+from channels.generic.websocket import WebsocketConsumer
+from src.helpers.socket_client import SocketClient
+
+class StingrayConsumer(WebsocketConsumer):
+    REFRESH_RATE = 1
+    # socket_client = SocketClient()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.socket_client = SocketClient()
+        self._message_thread = None
+        self._message_thread = threading.Thread(target=self.__message, name='message_thread')
+        self.is_running = True
+
+    def __message(self):
+        while self.is_running:
+            data = self.socket_client.get_data()
+            if data:
+                self.send(text_data=json.dumps(data))
+
+    def connect(self):
+        self.room_group_name = "test"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+        self.accept()
+        self._message_thread.start()
+
+    def disconnect(self, code):
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.is_running = False
+        self.socket_client.client.close()
+        print("DISCONNECT CODE: ",code)
--- commander/core/asgi.py
+++ commander/core/asgi.py
@@ -5,6 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import apps.chat.routing
+import apps.stingray.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
@@ -12,7 +13,8 @@ application = ProtocolTypeRouter(
     {
         "http": get_asgi_application(),
         "websocket": AuthMiddlewareStack(
-            URLRouter(apps.chat.routing.websocket_urlpatterns)
+            # URLRouter(apps.chat.routing.websocket_urlpatterns)
+            URLRouter(apps.stingray.routing.websocket_urlpatterns)
         ),
     }
 )
